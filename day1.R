8dee6149e5befdda9fc3739b9214e6570539b1bd
dbogdan <denisa.bogdan@icr.ac.uk>
2021-12-01 17:32:51 +0000

    day 1 solutions

--- day1.R
+++ day1.R
@@ -1,12 +1,16 @@
-depth <- read.delim("/Users/dbogdan/Documents/Advent_of_Code/input_day1.txt", header=F)
+depth <- read.delim("/Users/dbogdan/Documents/AoC_2021/input_day1.txt", header=F)
 depth <- depth$V1
 
 #Part 1
-inc <- 0
-for(i in 2:length(depth)) {
-  if(depth[i] > depth[i-1])
-    inc <- inc + 1
+count_increments <- function(depth) {
+  inc <- 0
+  for(i in 2:length(depth)) {
+    if(depth[i] > depth[i-1])
+      inc <- inc + 1
+  } 
+  return(inc)
 }
+count_increments(depth)
 
 #Part 2
 window <- c()
@@ -14,9 +18,4 @@ for(i in 1:(length(depth)-(length(depth) %% 3))) {
   sum <- depth[i] + depth[i+1] + depth[i+2]
   window <- c(window, sum)
 }
-
-inc <- 0
-for(j in 2:length(window)) {
-  if(window[j] > window[j-1])
-    inc <- inc + 1
-}
+count_increments(window)
