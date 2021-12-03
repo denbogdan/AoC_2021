2b4ad7bd17898ccc0ae074c5e1771828d490f4a0
dbogdan <denisa.bogdan@icr.ac.uk>
2021-12-03 12:06:32 +0000

    day 3 solutions

--- day1.R
+++ day1.R
@@ -1,7 +1,7 @@
 depth <- read.delim("/Users/dbogdan/Documents/AoC_2021/input_day1.txt", header=F)
 depth <- depth$V1
 
-#Part 1
+##Question 1 -----------------------------------------------------------------------
 count_increments <- function(depth) {
   inc <- 0
   for(i in 2:length(depth)) {
@@ -10,12 +10,14 @@ count_increments <- function(depth) {
   } 
   return(inc)
 }
+##Answer 1 -------------------------------------------------------------------------
 count_increments(depth)
 
-#Part 2
+##Question 2 -----------------------------------------------------------------------
 window <- c()
 for(i in 1:(length(depth)-(length(depth) %% 3))) {
   sum <- depth[i] + depth[i+1] + depth[i+2]
   window <- c(window, sum)
 }
+##Answer 2 -------------------------------------------------------------------------
 count_increments(window)
---
+++ day3.R
@@ -0,0 +1,77 @@
+library(stringr)
+rep <- read.delim("/Users/dbogdan/Documents/AoC_2021/input_day3.txt", header=F, colClasses = c("factor"))
+
+##Question 1 -----------------------------------------------------------------------
+n <- nrow(rep)
+rep <- sapply(rep$V1, function(x) str_extract_all(x, boundary("character"))) |> 
+  unlist() |> as.numeric() |> matrix(ncol=n) |> t()
+
+#gamma
+gamma <- c()
+for(i in 1:ncol(rep))
+{
+  if(sum(rep[,i]) > (n-sum(rep[,i])))
+    bit <- 1
+  else
+    bit <- 0
+  gamma <- c(gamma, bit)
+}
+
+#epsilon
+epsilon <- ifelse(gamma == 0, 1, 0)
+
+##Answer 1 -------------------------------------------------------------------------
+gamma_dec <- strtoi(paste0(as.character(gamma), collapse=""), base = 2)
+epsilon_dec <- strtoi(paste0(as.character(epsilon), collapse=""), base = 2)
+gamma_dec*epsilon_dec
+
+
+
+##Question 2 -----------------------------------------------------------------------
+oxygen <- rep
+i <- 1
+while(nrow(oxygen) > 1)
+{
+  #update n
+  n <- nrow(oxygen)
+  
+  #find most common digit
+  if(sum(oxygen[,i]) >= (n-sum(oxygen[,i])))
+    bit <- 1
+  else
+    bit <- 0
+  
+  #keep only rows that match the bit
+  oxygen <- oxygen[which(oxygen[,i] == bit),]
+  
+  #increment 
+  i <- i+1
+}
+
+CO2 <- rep
+i <- 1
+while(nrow(CO2) > 1)
+{
+  #update n
+  n <- nrow(CO2)
+  
+  #find most common digit
+  if(sum(CO2[,i]) >= (n-sum(CO2[,i])))
+    bit <- 0
+  else
+    bit <- 1
+  
+  #keep only rows that match the bit
+  CO2 <- CO2[which(CO2[,i] == bit),]
+  
+  #increment 
+  i <- i+1
+}
+
+
+##Answer 2 -------------------------------------------------------------------------
+oxygen_dec <- strtoi(paste0(as.character(oxygen), collapse=""), base = 2)
+CO2_dec <- strtoi(paste0(as.character(CO2), collapse=""), base = 2)
+oxygen_dec*CO2_dec
+
+
